4ea311028261fdd2adc72b637b441f22a00f732e
niyarabarla <nagalakshmi.yarabarla@skillsoft.com>
2020-01-03 15:20:26 +0530

    changes

--- src/main/java/com/qa/client/RestClient.java
+++ src/main/java/com/qa/client/RestClient.java
@@ -22,6 +22,7 @@ public class RestClient {
 	//1. GET Method without Headers:
 	public CloseableHttpResponse get(String url) throws ClientProtocolException, IOException{
 	CloseableHttpClient httpClient = HttpClients.createDefault();
+	
 	HttpGet httpget = new HttpGet(url); //http get request
 	CloseableHttpResponse closebaleHttpResponse =  httpClient.execute(httpget); //hit the GET URL
 	
