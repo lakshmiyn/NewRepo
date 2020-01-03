2a719534b7837c3c5b59c31ec3879d18026c29a8
niyarabarla <nagalakshmi.yarabarla@skillsoft.com>
2020-01-03 15:12:59 +0530

    addedOneLIne

--- src/main/java/com/qa/base/TestBase.java
+++ src/main/java/com/qa/base/TestBase.java
@@ -17,7 +17,9 @@ public class TestBase {
 	
 	
 	public TestBase(){
+
 		try {
+			
 			prop = new Properties();
 			FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+"/src/main/java/com/qa/config/config.properties");
 			prop.load(ip);
